c0ea896d56f822cce3bed14f1544b50d1853f9a4
Tpriest <852849893@qq.com>
2022-07-17 17:06:22 +0800

    git-test-basic初始化项目

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  let mainDom = document.getElementById("main");
+  if (mainDom) {
+    mainDom.addEventListener("click", function () {
+      alert("main首页的代码");
+    });
+  }
+};
